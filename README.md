ed79acb7957995bb44a1b572b3114b9943c472a0
Yaseen <fantasticyaseenshariff@gmail.com>
2022-03-22 18:42:33 +0530

    day 6

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 Link Available soon
 
 ## Day 2
+### Tip Calculator
 Status Pending
 
 ## Day 3
---
+++ codes/day6/karel.py
@@ -0,0 +1,105 @@
+# Day 6
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+
+# hurdle 4
+#
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+#
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+# last before one
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+# for step in range(6):
+#     jump()
+
+# 2
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+# move()
+# jump()
+# move()
+# jump()
+# move()
+# jump()
+# move()
+# jump()
+# move()
+# jump()
+# move()
+# jump()
+
+# 1
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# turn_left()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
+#
+
